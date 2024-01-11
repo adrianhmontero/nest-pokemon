dff53b7c8a2aa64dc4e9e048de86f224c64ecea3
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-01-10 23:34:14 -0600

    Modifying Pokemon entity to make a schema & adding its import in pokemon module

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,25 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+/* Para indicar que esta clase es un esquema de BD, usaremos el siguiente decorador: */
+@Schema()
+/* Al extender nuestra entidad/clase con Document de Mongoose, se añadirán todas las funcionalidades respectivas como nombres, métodos, etc.*/
+export class Pokemon extends Document {
+  // El id no necesitamos definirlo porque MongoDB generará uno internacionalmente único.
+
+  /* Con el decorador Prop vamos a indicar que el valor/campo de name debe ser único y que cuente con un index para que sea fácil de encontrar al hacer búsquedas. */
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+/* Hay que exportar un esquema para que, cuando se inicie la DB, indique que estas son las definiciones que use, las reglas, las columnas, etc. */
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  imports: [
+    /* Para que nuestros esquemas sean proporcionados adecuadamente y Mongoose cree las colecciones en la DB y que permita hacer la inyección de dependencias
+    para usar las clases en nuestros servicios, debemos hacer lo siguiente: */
+    MongooseModule.forFeature([{ name: Pokemon.name, schema: PokemonSchema }]),
+  ],
 })
 export class PokemonModule {}
